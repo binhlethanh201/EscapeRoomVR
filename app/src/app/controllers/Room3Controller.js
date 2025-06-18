6f592ba17177a6a45c7f084d63e52f1e2be120da
binhlethanh201 <ltbinh04@gmail.com>
2025-06-18 16:53:54 +0700

    panorama and minigame update

--- app/src/app/controllers/Room3Controller.js
+++ app/src/app/controllers/Room3Controller.js
@@ -12,12 +12,32 @@ class Room3Controller {
   painting(req, res, next) {
     res.render("room3/hotspot/painting");
   }
-   desk(req, res, next) {
+  desk(req, res, next) {
     res.render("room3/hotspot/desk");
   }
-    door(req, res, next) {
+  door(req, res, next) {
     res.render("room3/hotspot/door");
   }
-  
+
+  // Thêm route minigame fillInBlank
+  async fillInBlank(req, res, next) {
+  try {
+    const room = await Room3.findOne({ id: 'room3' }).exec();
+    if (!room) throw new Error('Room3 not found');
+
+    console.log('hotspots:', room.hotspots);  // DEBUG xem có trường window không
+
+    const windowHotspot = room.hotspots?.window;
+    if (!windowHotspot) throw new Error('Window hotspot not found');
+
+    const minigame = (windowHotspot.minigame || []).find(mg => mg.id === 'fillInBlank');
+    if (!minigame) throw new Error('FillInBlank minigame not found');
+
+    res.render('room3/minigame/fillInBlank', { minigame });
+  } catch (err) {
+    next(err);
+  }
+}
+
 }
 module.exports = new Room3Controller();
