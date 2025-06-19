79934efc1a36c3698792c7382a9deff47a627715
binhlethanh201 <ltbinh04@gmail.com>
2025-06-19 09:17:00 +0700

    room3 hotspots updated

--- app/src/routes/room3.js
+++ app/src/routes/room3.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const Room3Controller = require('../app/controllers/Room3Controller');
 
 router.get('/hotspot/window', Room3Controller.window);
+router.get('/minigame/fillInBlank', Room3Controller.fillInBlank);
 router.get('/hotspot/painting', Room3Controller.painting);
 router.get('/hotspot/desk', Room3Controller.desk);
 router.get('/hotspot/door', Room3Controller.door);
